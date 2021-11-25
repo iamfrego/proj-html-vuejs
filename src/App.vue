9c15882e659aacb2bbfb3100065c3c1b5025a33f
iamfrego <mate_freg@tiscali.it>
2021-11-25 11:49:57 +0100

    img add & bootstap installed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-
+    <Header/>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   components: {
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 
 </style>
