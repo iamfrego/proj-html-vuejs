daf979c3d1345704716c274c6b8c0456bbba068c
iamfrego <mate_freg@tiscali.it>
2021-11-25 16:18:31 +0100

    Error dynac image

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <body>
     <Header />
+    <Main />
   </body>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
   components: {
     Header,
+    Main,
   },
 };
 </script>
@@ -20,5 +23,6 @@ export default {
 
 body {
   font-family: "M PLUS 1 Code", sans-serif;
+  overflow-x: hidden;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="site_main">
+    <div class="img_container">
+      <div class="row">
+        <div class="col" v-for="n in 4" :key="n.i">
+          <img :src="`../assets/img/h3-img-` + n + `.jpg`" alt="" />
+        </div>
+      </div>
+    </div>
+    <div class="banner"></div>
+    <div class="offers"></div>
+    <div class="banner2"></div>
+    <div class="img_container"></div>
+    <div class="type"></div>
+    <div class="menu">
+      <div class="info"></div>
+      <div class="pizza_carousel"></div>
+    </div>
+    <div class="event"></div>
+    <div class="booking"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.img_container {
+  height: 225px;
+}
+.banner {
+  height: 400px;
+  background-repeat: repeat-x;
+  background-position-y: center;
+  background-image: url("../assets/img/h3-testimonials-bckgrnd.jpg");
+}
+</style>
